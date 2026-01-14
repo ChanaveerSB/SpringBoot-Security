c3498d0c20c06343b1be789889112a212831dc87
ChanaveerSB <chanaveersb@gmail.com>
2026-01-15 00:18:32 +0530

    Security_Layers

--- src/main/java/com/telusko/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/telusko/SpringSecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.telusko.SpringSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {  //HttpSecurity gives SecurityFilterChain object
         //http.build() gives SecurityFilterChain
+//        return http.build();
+
+        //Adding Security Layers :
+
+        //Disables CSRF protection, usually done for REST APIs where CSRF tokens are not required (e.g., Postman/JWT).
+        http.csrf(customizer -> customizer.disable());
+        //Ensures that every endpoint can be accessed only by authenticated users; no public URLs are allowed.
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        //Enables default form-based login provided by Spring Security (auto login page, username/password authentication).
+        http.formLogin(Customizer.withDefaults());
+        //Enables HTTP Basic authentication, where credentials are sent in the request header (commonly used for APIs and testing)
+        http.httpBasic(Customizer.withDefaults());
+
+
         return http.build();
     }
 
